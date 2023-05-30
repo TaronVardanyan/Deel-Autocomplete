a7bad4b83ddf95daa43de5a825f1d8ca63cc9519
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-30 19:49:19 +0400

    complete main logic of autocomplete input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, { lazy } from 'react';
+import { lazy, useState } from 'react';
 import { AutoCompleteInput } from './components';
 import './App.css';
 
 const ItemsList = lazy(() => import('./components/ItemsList'));
 
 function App() {
+  //setting loading state for client interface response visibility
+  const [isLoading, setLoadingState] = useState(false);
+
+  const handleUsername = (username: string) => {
+    if (username?.length) {
+      //call request api when username string has length
+
+      //set loading true on new username enter
+      setLoadingState(true);
+    } else {
+      setLoadingState(false);
+    }
+  };
+
   return (
     <div className="App">
-      <AutoCompleteInput />
+      <AutoCompleteInput handleUsername={handleUsername} isLoading={isLoading} />
       <ItemsList />
     </div>
   );
