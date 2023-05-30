e81bf4a8676e9900b69df999f72b78357de1f261
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-30 18:12:36 +0400

    add main components and initial view of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { lazy } from 'react';
+import { AutoCompleteInput } from './components';
 import './App.css';
 
+const ItemsList = lazy(() => import('./components/ItemsList'));
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <AutoCompleteInput />
+      <ItemsList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AutoCompleteInput/index.tsx
@@ -0,0 +1,7 @@
+import './AutoCompleteInput.css';
+
+const AutoCompleteInput = () => {
+  return <div className="input-wrapper">input</div>;
+};
+
+export default AutoCompleteInput;
